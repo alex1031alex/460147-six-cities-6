ed31e53facb247ff081eba0526cd7014c3a4d919
Alex <alex1031alex@yandex.ru>
2021-02-11 21:23:22 +0300

    3.1.1 Создаст моковые данные для предложений.

---
+++ src/mocks/offers.js
@@ -0,0 +1,230 @@
+export const offers = [
+  {
+    id: 1,
+    isPremium: true,
+    image: `img/apartment-01.jpg`,
+    price: 120,
+    isBookmarked: false,
+    rating: 4.8,
+    title: `Beautiful & luxurious apartment at great location`,
+    type: `Apartment`,
+    inside: [
+      {
+        name: `Wi-Fi`,
+        isAvailable: true,
+      },
+      {
+        name: `Heating`,
+        isAvailable: true,
+      },
+      {
+        name: `Kitchen`,
+        isAvailable: true,
+      },
+      {
+        name: `Fridge`,
+        isAvailable: true,
+      },
+      {
+        name: `Washing machine`,
+        isAvailable: true,
+      },
+      {
+        name: `Coffee machine`,
+        isAvailable: true,
+      },
+      {
+        name: `Dishwasher`,
+        isAvailable: true,
+      },
+      {
+        name: `Towels`,
+        isAvailable: true,
+      },
+      {
+        name: `Baby seat`,
+        isAvailable: true,
+      },
+      {
+        name: `Cabel TV`,
+        isAvailable: true,
+      }
+    ],
+    host: {
+      name: `Angelina`,
+      photo: `img/avatar-angelina.jpg`,
+      isPro: true
+    }
+  },
+  {
+    id: 2,
+    isPremium: false,
+    image: `img/room.jpg`,
+    price: 80,
+    isBookmarked: true,
+    rating: 4.0,
+    title: `Wood and stone place`,
+    type: `Private room`,
+    inside: [
+      {
+        name: `Wi-Fi`,
+        isAvailable: false,
+      },
+      {
+        name: `Heating`,
+        isAvailable: true,
+      },
+      {
+        name: `Kitchen`,
+        isAvailable: true,
+      },
+      {
+        name: `Fridge`,
+        isAvailable: true,
+      },
+      {
+        name: `Washing machine`,
+        isAvailable: true,
+      },
+      {
+        name: `Coffee machine`,
+        isAvailable: false,
+      },
+      {
+        name: `Dishwasher`,
+        isAvailable: false,
+      },
+      {
+        name: `Towels`,
+        isAvailable: true,
+      },
+      {
+        name: `Baby seat`,
+        isAvailable: false,
+      },
+      {
+        name: `Cabel TV`,
+        isAvailable: true,
+      }
+    ],
+    host: {
+      name: `Max`,
+      photo: `img/avatar-max.jpg`,
+      isPro: false
+    }
+  },
+  {
+    id: 3,
+    isPremium: false,
+    image: `img/apartment-02.jpg`,
+    price: 132,
+    isBookmarked: false,
+    rating: 4.7,
+    title: `Canal View Prinsengracht`,
+    type: `Apartment`,
+    inside: [
+      {
+        name: `Wi-Fi`,
+        isAvailable: false,
+      },
+      {
+        name: `Heating`,
+        isAvailable: false,
+      },
+      {
+        name: `Kitchen`,
+        isAvailable: false,
+      },
+      {
+        name: `Fridge`,
+        isAvailable: false,
+      },
+      {
+        name: `Washing machine`,
+        isAvailable: true,
+      },
+      {
+        name: `Coffee machine`,
+        isAvailable: false,
+      },
+      {
+        name: `Dishwasher`,
+        isAvailable: false,
+      },
+      {
+        name: `Towels`,
+        isAvailable: true,
+      },
+      {
+        name: `Baby seat`,
+        isAvailable: false,
+      },
+      {
+        name: `Cabel TV`,
+        isAvailable: true,
+      }
+    ],
+    host: {
+      name: `Max`,
+      photo: `img/avatar-max.jpg`,
+      isPro: false
+    }
+  },
+  {
+    id: 4,
+    isPremium: true,
+    image: `img/apartment-03.jpg`,
+    price: 180,
+    isBookmarked: false,
+    rating: 5.0,
+    title: `Nice, cozy, warm big bed apartment`,
+    type: `Apartment`,
+    inside: [
+      {
+        name: `Wi-Fi`,
+        isAvailable: false,
+      },
+      {
+        name: `Heating`,
+        isAvailable: true,
+      },
+      {
+        name: `Kitchen`,
+        isAvailable: true,
+      },
+      {
+        name: `Fridge`,
+        isAvailable: true,
+      },
+      {
+        name: `Washing machine`,
+        isAvailable: true,
+      },
+      {
+        name: `Coffee machine`,
+        isAvailable: true,
+      },
+      {
+        name: `Dishwasher`,
+        isAvailable: true,
+      },
+      {
+        name: `Towels`,
+        isAvailable: false,
+      },
+      {
+        name: `Baby seat`,
+        isAvailable: false,
+      },
+      {
+        name: `Cabel TV`,
+        isAvailable: true,
+      }
+    ],
+    host: {
+      name: `Max`,
+      photo: `img/avatar-max.jpg`,
+      isPro: false
+    }
+  }
+];
