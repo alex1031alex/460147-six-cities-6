f5f41148645d9be52033ee3278d264677dc57b4d
Alex <alex1031alex@yandex.ru>
2021-02-12 21:09:22 +0300

    3.1.4 Отрисует компонент предложения на основе моковых данных

--- src/components/offer/offer.jsx
+++ src/components/offer/offer.jsx
@@ -1,39 +1,59 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Offer = () => {
+const Offer = (props) => {
+  const {offer} = props;
+  const {image, price, isBookmarked, rating, title, type} = offer;
+
   return (
     <article className="cities__place-card place-card">
       <div className="cities__image-wrapper place-card__image-wrapper">
         <a href="#">
-          <img className="place-card__image" src="img/room.jpg" width={260} height={200} alt="Place image" />
+          <img className="place-card__image" src={image} width={260} height={200} alt="Place image" />
         </a>
       </div>
       <div className="place-card__info">
         <div className="place-card__price-wrapper">
           <div className="place-card__price">
-            <b className="place-card__price-value">€80</b>
+            <b className="place-card__price-value">€{price}</b>
             <span className="place-card__price-text">/&nbsp;night</span>
           </div>
-          <button className="place-card__bookmark-button place-card__bookmark-button--active button" type="button">
+          <button
+            type="button"
+            className={`place-card__bookmark-button button
+            ${isBookmarked ? `place-card__bookmark-button--active` : ``}`}
+          >
             <svg className="place-card__bookmark-icon" width={18} height={19}>
               <use xlinkHref="#icon-bookmark" />
             </svg>
-            <span className="visually-hidden">In bookmarks</span>
+            <span className="visually-hidden">${isBookmarked ? `In` : `To`} bookmarks</span>
           </button>
         </div>
         <div className="place-card__rating rating">
           <div className="place-card__stars rating__stars">
-            <span style={{width: `80%`}} />
+            <span style={{width: `${Math.round(rating) * 20}%`}} />
             <span className="visually-hidden">Rating</span>
           </div>
         </div>
         <h2 className="place-card__name">
-          <a href="#">Wood and stone place</a>
+          <a href="#">{title}</a>
         </h2>
-        <p className="place-card__type">Private room</p>
+        <p className="place-card__type">{type}</p>
       </div>
     </article>
   );
+};
+
+Offer.propTypes = {
+  offer: PropTypes.shape({
+    isPremium: PropTypes.bool.isRequired,
+    image: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+    isBookmarked: PropTypes.bool.isRequired,
+    rating: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired,
+  }).isRequired,
 };
 
 export default Offer;
