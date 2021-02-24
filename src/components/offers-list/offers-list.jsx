320dd29d9611af32ad9b277f6594259f399eb7fe
Alex <alex1031alex@yandex.ru>
2021-02-24 23:39:58 +0300

    4.2.4 Установит пакет classnames, импортирует его в компоненте списка предложений.

--- src/components/offers-list/offers-list.jsx
+++ src/components/offers-list/offers-list.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import {useState} from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
+import cn from 'classnames';
 
 import Offer from '../offer/offer.jsx';
 
