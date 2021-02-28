4aba3287a5e470a325f33d86ee6c1f5605cf839a
Alex <alex1031alex@yandex.ru>
2021-02-28 20:49:01 +0300

    5.1.3 Подключит Redux к проекту. Создаст хранилище.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+
+import {reducer} from './store/reducer.js';
 import App from './components/app/app.jsx';
 import {offers} from './mocks/offers.js';
 import {reviews} from './mocks/reviews.js';
 
 const OFFERS_COUNT = 5;
 
+const store = createStore(reducer);
+
 ReactDOM.render(
-    <App
-      offersCount={OFFERS_COUNT}
-      offers={offers}
-      reviews={reviews}
-    />,
+    <Provider store={store}>
+      <App
+        offersCount={OFFERS_COUNT}
+        offers={offers}
+        reviews={reviews}
+      />
+    </Provider>,
     document.querySelector(`#root`)
 );
