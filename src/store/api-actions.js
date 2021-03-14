e210d41d28af7ffab8968d35a660a0f7969cda4d
Alex <alex1031alex@yandex.ru>
2021-03-14 11:22:18 +0300

    6.2.1 Создаст асинхронные действия для проверки авторизации пользователя и логина.

--- src/store/api-actions.js
+++ src/store/api-actions.js
@@ -1,8 +1,23 @@
 import {ActionCreator} from './action.js';
 import {adaptOffersData} from '../services/adapter.js';
+import { AuthorizationStatus } from '../const.js';
 
 export const fetchOffers = () => (dispatch, _getState, api) => {
   api.get(`/hotels`)
     .then(({data}) => adaptOffersData(data))
     .then((data) => dispatch(ActionCreator.loadOffers(data)));
 };
+
+export const checkAuth = () => (dispath, _getState, api) => (
+  api.get(`/login`)
+    .then(() => dispath(ActionCreator.requireAuthorization(AuthorizationStatus.AUTH)))
+    .catch(() => {})
+);
+
+export const login = ({email, password}) => (dispatch, _getState, api) => (
+  api.post(`/login`, {email, password})
+    .then(({data}) => {
+      dispatch(ActionCreator.requireAuthorization(AuthorizationStatus.AUTH));
+      return data;
+    })
+);
