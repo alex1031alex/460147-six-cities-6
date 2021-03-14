b75929ece1bb45adfdc14830dbc4a896f67e57a8
Alex <alex1031alex@yandex.ru>
2021-03-14 16:07:54 +0300

    6.2.5 Вынесет отрисовки шапки приложения в отдельный компонент Header.

---
+++ src/components/header/header.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+import {Link} from 'react-router-dom';
+
+const Header = (props) => {
+  const {authInfo} = props;
+
+  return (
+    <header className="header">
+      <div className="container">
+        <div className="header__wrapper">
+          <div className="header__left">
+            <a className="header__logo-link header__logo-link--active">
+              <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width={81} height={41} />
+            </a>
+          </div>
+          <nav className="header__nav">
+            <ul className="header__nav-list">
+              <li className="header__nav-item user">
+                <Link className="header__nav-link header__nav-link--profile" to={`/favorites`}>
+                  <div className="header__avatar-wrapper user__avatar-wrapper">
+                  </div>
+                  <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+const mapStateToProps = (state) => ({
+  authInfo: state.authInfo,
+});
+
+Header.propTypes = {
+  authInfo: PropTypes.shape({
+    avatarUrl: PropTypes.string,
+    email: PropTypes.string,
+    id: PropTypes.number,
+    isPro: PropTypes.bool,
+    name: PropTypes.string,
+  }),
+};
+
+export {Header};
+export default connect(mapStateToProps, null)(Header);
+
